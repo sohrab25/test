d31f37ae279be75015c20ab9a6a8ba6d99a5956d
sohrab25 <sohrabhpps@gmail.com>
2017-07-07 02:11:42 +0600

    all html files

---
+++ new-layout2 11.6.17/js/slider/nivo-active.js
@@ -0,0 +1,15 @@
+(function ($) {
+"use strict";
+/*Home main */
+	$('#mainSlider').nivoSlider({
+		directionNav: true,
+		animSpeed: 500,
+		slices: 18,
+		pauseTime: 5000,
+		pauseOnHover: false,
+		controlNav: true,
+		prevText: '<i class="fa fa-angle-left nivo-prev-icon"></i>',
+		nextText: '<i class="fa fa-angle-right nivo-next-icon"></i>'
+	});
+
+})(jQuery);	
